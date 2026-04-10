165a6fa2f390a9a60a44c54f2ef4a9fed0691e3c
DEBANJANAB991 <debanjona.b.bhattacharjya@fau.de>
2026-04-10 22:32:44 +0200

    Updated config file added

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-# config.py  (root of your repo)
+# config.py 
 
 from pathlib import Path
 
 # ------------------------------------------------------------------
-# Mandatory paths  – adjust if your data live somewhere else
+# Mandatory paths 
 # ------------------------------------------------------------------
 DATASET_PATH   = Path("/home/vault/iwi5/iwi5293h/CT-Motion-Artifact-Reduction_bkp/data/dicom_raw")
 CLEAN_SINOGRAM_ROOT=Path("/home/woody/iwi5/iwi5293h/clean_sinograms") #clean sinogram in 3D generated from dicom files
@@ -11,13 +11,16 @@ TEST_CLEAN_SINOGRAM = Path("/home/woody/iwi5/iwi5293h/clean_sinograms_test") #te
 CLEAN_SINOGRAM_2D=Path("/home/woody/iwi5/iwi5293h/clean_sinograms_2d") #clean sinogram in 2D generated from CLEAN_SINOGRAM_ROOT
 CLEAN_SINOGRAM_2D_TEST=Path("/home/woody/iwi5/iwi5293h/clean_sinograms_2d_test") #test clean sinogram in 2D generated from TEST_CLEAN_SINOGRAM
 ARTIFACT_ROOT_2D=Path("/home/woody/iwi5/iwi5293h/art_sinograms_2d_v2")#artifact in 2d generated from CLEAN_SINOGRAM_2D by adding motion artifacts
-ARTIFACT_SINOGRAM_2D_TEST_v2=Path("/home/woody/iwi5/iwi5293h/art_sinograms_2d_test_v2")# test artifacts in 2d generated from CLEAN_SINOGRAM_2D_TEST by adding motion artifacts
+ARTIFACT_SINOGRAM_3D_TEST=Path("/home/woody/iwi5/iwi5293h/art_sinograms_3D_test")
 PREDICTED_SINOGRAM_2D_TEST_v2=Path("/home/woody/iwi5/iwi5293h/predicted_sinograms_2d_test_v2") # test predicted sinogram in 2D generated after testing using MR_LKV on ARTIFACT_SINOGRAM_2D_TEST_v2
 MERGED_SINOGRAM_3D_TEST_v2=Path("/home/woody/iwi5/iwi5293h/merged_sinograms_3d_test_v2") # test merged sinogram in 3D generated after merging predicted 2D sinograms from PREDICTED_SINOGRAM_2D_TEST_v2
 MERGED_ARTIFACT_3D_TEST_v2=Path("/home/woody/iwi5/iwi5293h/merged_artifact_3d_test_v2") # test merged artifact sinogram in 3D generated after merging artifact 2D sinograms from ARTIFACT_SINOGRAM_2D_TEST_v2
-#ARTIFACTED_SINOGRAM_3D=Path("/home/woody/iwi5/iwi5293h/art_sinograms") # 3D artifacted sinograms generated from CLEAN_SINOGRAM_ROOT by adding motion artifacts
-ARTIFACT_ROOT=Path("/home/woody/iwi5/iwi5293h/art_sinograms") #artifact in 3D generated from CLEAN_SINOGRAM_ROOT by adding motion artifacts
-TEST_ARTIFACTED_SINOGRAM=Path("/home/woody/iwi5/iwi5293h/art_sinograms_test") # test 3D artifacted sinograms generated from TEST_CLEAN_SINOGRAM by adding motion artifacts
+RECONSTRUCTED_CT_VOLUME  =Path("/home/woody/iwi5/iwi5293h/reconstructed_ct_volumes") #reconstructed ct volume generated
+ART_SINOGRAM_2D=Path("/home/woody/iwi5/iwi5293h/art_sinograms_2d") # new artifact in 2D generated from CLEAN_SINOGRAM_2D_TEST by adding motion artifacts using a different random seed
+ARTIFACT_SINOGRAM_ROOT=Path("/home/woody/iwi5/iwi5293h/artifact_sinograms") #artifact in 3D generated from CLEAN_SINOGRAM_ROOT by adding motion artifacts
+RECON_CLEAN_ROOT = Path("/home/woody/iwi5/iwi5293h/recon_clean") #reconstructed ct volume generated from clean sinograms
+RECON_ARTIFACT_ROOT = Path("/home/woody/iwi5/iwi5293h/recon_artifact") #reconstructed ct volume generated from artifact sinograms
+
 # Where to save model checkpoints (scratch)
 
 CKPT_DIR=Path("/home/vault/iwi5/iwi5293h/models")
@@ -45,12 +48,11 @@ EPOCHS = 100
 MU_WATER = 0.019 
 
 # Batch size per GPU
-BATCH_SIZE = 16
 BATCH_SIZE=8
 
 
 # Learning rate for optimizer
-LR = 1e-3
+LR = 1e-4
 
 # How often (in epochs) to save a checkpoint
 SAVE_INTERVAL = 10
